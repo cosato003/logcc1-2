31bf1987aaca34010513d3be3d0f429eba94b518
Juanita <juanitacorrea9@hotmail.com>
2023-08-19 12:56:53 -0500

    calculadora con ciclo

--- logica.java
+++ logica.java
@@ -3,28 +3,39 @@ import java.util.Scanner;
 class logica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
 
-        System.out.println("Ingresa operador 1:");
-        int operador1 = scanner.nextInt();
+        while (continuar) {
+            System.out.println("Ingresa operador 1:");
+            int operador1 = scanner.nextInt();
 
-        System.out.println("Ingresa operador 2:");
-        int operador2 = scanner.nextInt();
+            System.out.println("Ingresa operador 2:");
+            int operador2 = scanner.nextInt();
 
-        int suma = operador1 + operador2;
-        System.out.println("El resultado de la suma es: " + suma);
+            int suma = operador1 + operador2;
+            System.out.println("El resultado de la suma es: " + suma);
 
-        int resta = operador1 - operador2;
-        System.out.println("El resultado de la resta es: " + resta);
+            int resta = operador1 - operador2;
+            System.out.println("El resultado de la resta es: " + resta);
 
-        if (operador2 != 0) {
-            double division = (double) operador1 / operador2;
-            System.out.println("El resultado de la división es: " + division);
-        } else {
-            System.out.println("No es posible dividir por 0.");
+            if (operador2 != 0) {
+                double division = (double) operador1 / operador2;
+                System.out.println("El resultado de la división es: " + division);
+            } else {
+                System.out.println("No es posible dividir por 0.");
+            }
+
+            int multiplicacion = operador1 * operador2;
+            System.out.println("El resultado de la multiplicación es: " + multiplicacion);
+
+            System.out.println("¿Quieres hacer otra operación? (s/n)");
+            String respuesta = scanner.next();
+
+            if (respuesta.equalsIgnoreCase("n")) {
+                continuar = false;
+            }
         }
-
-        int multiplicacion = operador1 * operador2;
-        System.out.println("El resultado de la multiplicación es: " + multiplicacion);
         scanner.close();
+        System.out.println("Hasta luego");
     }
 }
